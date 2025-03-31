ce221ad59452403ed3501f6ddcea54fc01abcaca
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-31 18:46:12 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,6 +1,6 @@
 import re
 from datetime import datetime
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
@@ -8,9 +8,25 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/hello/<name>",methods=["GET", "POST"])
-def hello_there(name='Lilian Johanna'):
-    return render_template(
-        "signup_form.html",
-        name=name,
-        date=datetime.now()
-    )
+#def hello_there(name='Lilian Johanna'):
+    #return render_template(
+        #"signup_form.html",
+        #name=name,
+        #date=datetime.now()
+    #)
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('name')
+        framework = request.form.get('email')
+        return '''
+                  <h1>The name value is: {}</h1>
+                  <h1>The email value is: {}</h1>'''.format(language, framework)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>Language: <input type="text" name="language"></label></div>
+               <div><label>Framework: <input type="text" name="framework"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
