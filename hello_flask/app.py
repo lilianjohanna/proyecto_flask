b2fd3edea6954f72df61870263872d98d18ea1b1
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-31 20:39:42 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -34,7 +34,7 @@ def hello_there():
                     <input type="submit" value="Cerrar">
                     <h4>Tipo de documento: {}</h4>
                     <h4>Número de documento: {}</h4>'''.format(language, framework)+'''
-                    <p>Registro encontrado: '''+{f"listaresultados[0]['body']"}
+                    <p>Registro encontrado: '''+{listaresultados[4]}
         else:
             #print('Error en la solicitud, detalles:', response.text)
             return '''
