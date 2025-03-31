3bfb72349a21c00e99e98c83243e2941e5710879
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-31 19:12:21 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -20,12 +20,13 @@ def hello_there():
         language = request.form.get('name')
         framework = request.form.get('email')
         return '''
+                <form action="#">
                   <h4>Tipo de documento: {}</h4>
                   <h4>Número de documento: {}</h4>'''.format(language, framework)
     else:
         # otherwise handle the GET request
         return '''
-           <form method="POST">
+           <form action="#" method="POST">
                <div><label>Tipo de documento: <input type="text" name="name"></label></div>
                <div><label>Número de documento: <input type="number" name="email"></label></div>
                <input type="submit" value="Submit">
