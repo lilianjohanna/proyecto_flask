2dbf45f712f67d9b53df4e2313d360cbd0d8b9a7
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-31 20:46:25 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -31,9 +31,8 @@ def hello_there():
                     <div><label>Tipo de documento: <input type="text" name="name" readonly></label></div>
                     <div><label>Número de documento: <input type="text" name="email" readonly></label></div>
                     <input type="submit" value="Cerrar">
-                    <h4>Body: {listaresultados[4]}</h4>
                     <h4>Tipo de documento: {}</h4>
-                    <h4>Número de documento: {}</h4>'''.format(language, framework)
+                    <h4>Número de documento: {}</h4>'''.format(language, framework)+{listaresultados[list]}
         else:
             #print('Error en la solicitud, detalles:', response.text)
             return '''
