39509f43895e2bfeb59dd59e84df1b23da02ac98
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-04-01 20:31:30 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,15 +1,4 @@
-import re
-from datetime import datetime
-from flask import Flask, request, jsonify, render_template
 import requests
-import json
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
-
-app = Flask(__name__)
-jwt = JWTManager(app)
-
-app = Flask(__name__)
-
 def hello_there():
     auth_url = 'https://vanti.ecs-la.com/VantiListoServicesNMTest/api/Auth/LoginApi'
     datos={"username":"info@adndigital.co","password":"Vanti2023*"}
