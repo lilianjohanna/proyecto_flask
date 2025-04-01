b1d57505329bf3b8688663ad16aaad803f290a9d
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-04-01 20:32:56 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,10 +1,9 @@
 import requests
-def hello_there():
-    auth_url = 'https://vanti.ecs-la.com/VantiListoServicesNMTest/api/Auth/LoginApi'
-    datos={"username":"info@adndigital.co","password":"Vanti2023*"}
-    response = requests.post(auth_url,json=datos)
-    if response.status_code == 200:
-        token=response.json().get("token")
-        print("Token obtenido: ", token)
-    else:
-        print(f"Error al obtener el token: {response.status_code} - {response.text}")
+auth_url = 'https://vanti.ecs-la.com/VantiListoServicesNMTest/api/Auth/LoginApi'
+datos={"username":"info@adndigital.co","password":"Vanti2023*"}
+response = requests.post(auth_url,json=datos)
+if response.status_code == 200:
+    token=response.json().get("token")
+    print("Token obtenido: ", token)
+else:
+    print(f"Error al obtener el token: {response.status_code} - {response.text}")
