e1432837a069246aa8c4de561f2890d85a9b0335
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-04-01 20:27:40 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -14,23 +14,11 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/hello/<name>", methods=['GET', 'POST'])
 def hello_there():
-    # handle the POST request
-    if request.method == 'POST':
-        language = request.form.get('name')
-        framework = request.form.get('email')
-        auth_url = 'https://vanti.ecs-la.com/VantiListoServicesNMTest/api/Auth/LoginApi'
-        datos={"username":"info@adndigital.co","password":"Vanti2023*"}
-        response = requests.post(auth_url,json=datos)
-        if response.status_code == 200:
-            token=response.json().get("token")
-            print("Token obtenido: ", token)
-        else:
-            print(f"Error al obtener el token: {response.status_code} - {response.text}")
+    auth_url = 'https://vanti.ecs-la.com/VantiListoServicesNMTest/api/Auth/LoginApi'
+    datos={"username":"info@adndigital.co","password":"Vanti2023*"}
+    response = requests.post(auth_url,json=datos)
+    if response.status_code == 200:
+        token=response.json().get("token")
+        print("Token obtenido: ", token)
     else:
-        # otherwise handle the GET request
-        return '''
-           <form method="POST">
-               <div><label>Tipo de documento: <input type="text" name="name"></label></div>
-               <div><label>Número de documento: <input type="text" name="email"></label></div>
-               <input type="submit" value="Submit">
-           </form>'''
+        print(f"Error al obtener el token: {response.status_code} - {response.text}")
