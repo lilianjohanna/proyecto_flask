69c36b48596fd17e455c01f24fc133ecf92f29e1
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-04-07 17:28:39 +0000

    ...

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -21,17 +21,14 @@ def hello_there():
         URL = 'https://www.datos.gov.co/resource/6cat-2gcs.json?nit='+language+'&supervisor='+framework
         response = requests.get(URL)
         if response.status_code == 200:
-            listaresultados=json.loads(json.stringify(response.json()))
-            nit = listaresultados["nit"]+'</br>'
-            razonsocial = listaresultados["raz_n_social"]+'</br>'
-            supervisor = listaresultados["departamento_domicilio"]+'</br>'
+            listaresultados=json.dumps(response.json())
             return '''
                 <form action="javascript:window.close();">
                     <div><label>NIT: <input type="text" name="name" readonly></label></div>
                     <div><label>Supervisor: <input type="text" name="email" readonly></label></div>
                     <input type="submit" value="Cerrar">
                     <h4>Tipo de documento: {}</h4>
-                    <h4>Número de documento: {}</h4>'''.format(language, framework)+nit+razonsocial+supervisor
+                    <h4>Número de documento: {}</h4>'''.format(language, framework)+listaresultados
         else:
             #print('Error en la solicitud, detalles:', response.text)
             return '''
